ea0bff63810f6c817a18fdd64440013eb07890bc
Ricardo <128722871+Flexzay@users.noreply.github.com>
2024-07-10 10:21:11 -0500

    Delete Controladores directory

--- Controladores/controlador_Productos.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-include('../Modelos/add_productos.php');
-
-add_productos::add_productos();     
--- Controladores/controlador_eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-include('../Modelos/delete_productos.php');
-
-delete_productos::delete_productos();
-?>
